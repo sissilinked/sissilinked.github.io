c39458e14ded34c2a3a76c4600512e345d2b7208
marcograsso <56439271+marcograsso@users.noreply.github.com>
2020-06-14 10:30:06 +0200

    form2

---
+++ getintouch.php
@@ -0,0 +1,15 @@
+<?php
+if(!empty($_POST["submit"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$content = $_POST["message"];
+
+	$toEmail = "sissilinked@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "getintouch.php";
+?>
